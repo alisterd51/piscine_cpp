8fa081c8e95c7bb2df480c505eda80a71d3d8402
anclarma <anclarma@student.42.fr>
2021-12-17 15:30:56 +0100

    correction lignes vide

--- cpp06/ex02/Base.hpp
+++ cpp06/ex02/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: anclarma <anclarma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 12:50:31 by anclarma          #+#    #+#             */
-/*   Updated: 2021/11/16 12:58:50 by anclarma         ###   ########.fr       */
+/*   Updated: 2021/12/17 15:30:41 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,18 @@ class	Base
 {
 	public:
 		virtual	~Base(void);
-	protected:
-
-	private:
-		
 };
 
 class	A : public Base
 {
-	public:
-
-	protected:
-
-	private:
-		
 };
 
 class	B : public Base
 {
-	public:
-
-	protected:
-
-	private:
-		
 };
 
 class	C : public Base
 {
-	public:
-
-	protected:
-
-	private:
-		
 };
 
 Base	*generate(void);
